7926cdbdf31308bc5b484edc2cb0a3caf6bc6dce
Andrey Khramov <159222638+mzpdqk@users.noreply.github.com>
2024-11-15 16:38:57 +0400

    6213-100503D Храмов Андрей Александрович Лаб.2 Вар.10

---
+++ main.py
@@ -0,0 +1,67 @@
+import csv
+import argparse
+import os
+from typing import List
+from icrawler.builtin import GoogleImageCrawler
+
+def fetch_imgs(term: str, folder: str, max_imgs: int = 100) -> List[str]:
+    """качаем изображения"""
+    crawler = GoogleImageCrawler(storage={"root_dir": folder})
+    crawler.crawl(keyword=term, max_num=max_imgs)
+
+    return [os.path.join(folder, f) for f in os.listdir(folder)
+            if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+
+def save_ann(img_paths: List[str], ann_file: str):
+    """записываем аннотации в csv файл"""
+    with open(ann_file, mode='w', newline='', encoding='utf-8') as f:  # добавлено encoding='utf-8'
+        writer = csv.writer(f)
+        writer.writerow(['abs_path', 'rel_path'])
+        for img in img_paths:
+            writer.writerow([os.path.abspath(img), os.path.relpath(img, start=os.path.dirname(ann_file))])
+
+class ImgLoader:
+    def __init__(self, ann_file: str):
+        """инициализируем загрузчик"""
+        self.ann_file = ann_file
+        self.img_paths = []
+        self.index = 0
+        self.load_ann()
+
+    def load_ann(self):
+        """загружаем аннотации из csv файла"""
+        with open(self.ann_file, mode='r', encoding='utf-8') as f:  # добавлено encoding='utf-8'
+            reader = csv.reader(f)
+            next(reader)  # пропускаем заголовок
+            self.img_paths = [row[0] for row in reader]
+
+    def __iter__(self):
+        """вернули итератор"""
+        return self
+
+    def __next__(self) -> str:
+        """возвращаем следующий путь к изображению"""
+        if self.index < len(self.img_paths):
+            img_path = self.img_paths[self.index]
+            self.index += 1
+            return img_path
+        else:
+            raise StopIteration
+
+def main():
+    parser = argparse.ArgumentParser(description="загрузка изображений.")
+    parser.add_argument("term", type=str, help="поисковый запрос")
+    parser.add_argument("folder", type=str, help="папка для загрузки")
+    parser.add_argument("ann_file", type=str, help="файл для аннотаций")
+    args = parser.parse_args()
+
+    os.makedirs(args.folder, exist_ok=True)
+    img_paths = fetch_imgs(args.term, args.folder)  # порядок ввода в консоль: "keyword  папка  аннотация.csv"
+    save_ann(img_paths, args.ann_file)
+
+    img_loader = ImgLoader(args.ann_file)
+    for img_path in img_loader:
+        print(img_path)
+
+if __name__ == "__main__":
+    main()
